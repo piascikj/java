3a8e29b0b0ef08027b79970ed7cb6c971f28ec2c
rstiller <robert.stiller.mail@googlemail.com>
2013-01-20 00:09:34 +0100

    adjusted spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require 'puppet'
 require 'rubygems'
 require 'rspec-puppet'
-require 'puppetlabs_spec_helper/puppet_spec_helper'
 
 RSpec.configure do |c|
   c.module_path = File.join(File.dirname(__FILE__), '..')
